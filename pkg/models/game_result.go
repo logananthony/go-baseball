2d2bc2e8d21f664121ecc7dfdd4044cef87675b5
Logan Anthony <logananthonymlb@gmail.com>
2025-03-28 00:29:28 -0500

    Adding PlateAppearanceResult to GameResult struct

--- pkg/models/game_result.go
+++ pkg/models/game_result.go
@@ -3,10 +3,12 @@ package models
 
 type GameResult struct {
 
+    GameYear int
     HomeScore int
     AwayScore int
     Inning int
     InningTopBot string
+    PitchData []PlateAppearanceResult
 
     
   
