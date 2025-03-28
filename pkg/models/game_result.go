1008bf603a21bb9f6de017fd25a3b5e6f4634cd6
Logan Anthony <logananthonymlb@gmail.com>
2025-03-28 01:01:41 -0500

    Adding a few more fields to the GameResult struct

--- pkg/models/game_result.go
+++ pkg/models/game_result.go
@@ -4,10 +4,15 @@ package models
 type GameResult struct {
 
     GameYear int
-    HomeScore int
     AwayScore int
+    HomeScore int
+    AtBatNumber int
     Inning int
     InningTopBot string
+    Outs int
+    On1b bool
+    On2b bool 
+    On3b bool
     PitchData []PlateAppearanceResult
 
     
