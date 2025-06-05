7edf95462567c0e2987c4c843df6cbb4d0878f61
Logan Anthony <logananthonymlb@gmail.com>
2025-06-04 21:08:02 -0700

    Hardcode port 8080 for Render

--- pkg/models/game_data.go
+++ pkg/models/game_data.go
@@ -1,88 +1,9 @@
 package models
 
 type GameData struct {
-	// Home Batters
-	HomeBatter1Id       int
-	HomeBatter1GameYear int
-	HomeBatter2Id       int
-	HomeBatter2GameYear int
-	HomeBatter3Id       int
-	HomeBatter3GameYear int
-	HomeBatter4Id       int
-	HomeBatter4GameYear int
-	HomeBatter5Id       int
-	HomeBatter5GameYear int
-	HomeBatter6Id       int
-	HomeBatter6GameYear int
-	HomeBatter7Id       int
-	HomeBatter7GameYear int
-	HomeBatter8Id       int
-	HomeBatter8GameYear int
-	HomeBatter9Id       int
-	HomeBatter9GameYear int
-
-	// Home Pitchers
-	HomeStartingPitcherId       int
-	HomeStartingPitcherGameYear int
-	HomeBullpen1Id              int
-	HomeBullpen1GameYear        int
-	HomeBullpen2Id              int
-	HomeBullpen2GameYear        int
-	HomeBullpen3Id              int
-	HomeBullpen3GameYear        int
-	HomeBullpen4Id              int
-	HomeBullpen4GameYear        int
-	HomeBullpen5Id              int
-	HomeBullpen5GameYear        int
-	HomeBullpen6Id              int
-	HomeBullpen6GameYear        int
-	HomeBullpen7Id              int
-	HomeBullpen7GameYear        int
-	HomeBullpen8Id              int
-	HomeBullpen8GameYear        int
-	HomeBullpen9Id              int
-	HomeBullpen9GameYear        int
-
-	// Away Batters
-	AwayBatter1Id       int
-	AwayBatter1GameYear int
-	AwayBatter2Id       int
-	AwayBatter2GameYear int
-	AwayBatter3Id       int
-	AwayBatter3GameYear int
-	AwayBatter4Id       int
-	AwayBatter4GameYear int
-	AwayBatter5Id       int
-	AwayBatter5GameYear int
-	AwayBatter6Id       int
-	AwayBatter6GameYear int
-	AwayBatter7Id       int
-	AwayBatter7GameYear int
-	AwayBatter8Id       int
-	AwayBatter8GameYear int
-	AwayBatter9Id       int
-	AwayBatter9GameYear int
-
-	// Away Pitchers
-	AwayStartingPitcherId       int
-	AwayStartingPitcherGameYear int
-	AwayBullpen1Id              int
-	AwayBullpen1GameYear        int
-	AwayBullpen2Id              int
-	AwayBullpen2GameYear        int
-	AwayBullpen3Id              int
-	AwayBullpen3GameYear        int
-	AwayBullpen4Id              int
-	AwayBullpen4GameYear        int
-	AwayBullpen5Id              int
-	AwayBullpen5GameYear        int
-	AwayBullpen6Id              int
-	AwayBullpen6GameYear        int
-	AwayBullpen7Id              int
-	AwayBullpen7GameYear        int
-	AwayBullpen8Id              int
-	AwayBullpen8GameYear        int
-	AwayBullpen9Id              int
-	AwayBullpen9GameYear        int
+	HomeTeam            string
+	AwayTeam            string
+	HomeStartingPitcher int
+	AwayStartingPitcher int
+	GameYear            int
 }
-
