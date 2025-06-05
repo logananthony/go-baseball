94faba059b75f2bcb5e0c726d54bdd1235a3658f
Logan Anthony <logananthonymlb@gmail.com>
2025-06-05 16:44:53 -0700

    fix(docker): ensure CSV data files included in image

--- pkg/fetcher/fetch_batter_contact_percentage_league.go
+++ pkg/fetcher/fetch_batter_contact_percentage_league.go
@@ -1,26 +1,26 @@
 package fetcher
 
 import (
-    "path/filepath"
-    "os"
-    "github.com/logananthony/go-baseball/pkg/models"
-    "github.com/gocarina/gocsv"
+	"os"
+	"path/filepath"
+
+	"github.com/gocarina/gocsv"
+	"github.com/logananthony/go-baseball/pkg/models"
 )
 
 func FetchBatterContactPercentageLeague() []models.BatterContactPercentageLeague {
-    cwd, _ := os.Getwd()
-    path := filepath.Join(cwd, "pkg", "fetcher", "data", "batter_contact_percentages_league.csv")
-    csvFile, err := os.Open(path)
-    if err != nil {
-        panic(err)
-    }
-    defer csvFile.Close()
+	cwd, _ := os.Getwd()
+	path := filepath.Join(cwd, "pkg", "fetcher", "data", "batter_contact_percentages_league.csv")
+	csvFile, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer csvFile.Close()
 
-    var records []models.BatterContactPercentageLeague
-    if err := gocsv.UnmarshalFile(csvFile, &records); err != nil {
-        panic(err)
-    }
+	var records []models.BatterContactPercentageLeague
+	if err := gocsv.UnmarshalFile(csvFile, &records); err != nil {
+		panic(err)
+	}
 
-    return records
+	return records
 }
-
